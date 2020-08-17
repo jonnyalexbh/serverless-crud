b9c59ae6f556e0705294029ff9ad8a616caad592
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-08-17 14:12:49 -0500

    add README.md and initial refactor

---
+++ README.md
@@ -0,0 +1,9 @@
+# Serverless Framework CRUD
+
+**Create project**
+
+```sh
+$ sls create -t aws-nodejs -p serverless-crud
+$ sls invoke local -f hello
+$ sls invoke local -f hello -d '{"name": "jonnyalexbh"}'
+```
--- handler.js
+++ handler.js
@@ -3,16 +3,6 @@
 module.exports.hello = async event => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify({ data: 'info initial' }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
